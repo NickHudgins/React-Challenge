1ff3a395fb646ae13b0e18642530333e85d540a4
NickHudgins <nickhudgins@gmail.com>
2020-02-17 09:15:19 -0500

    Top banner image formatting progress.

--- src/Components/TopBanner/DogsCard.js
+++ src/Components/TopBanner/DogsCard.js
@@ -7,18 +7,21 @@ import "./TopBanner.css";
 function DogsCard() {
   return (
     <Card>
-      <div >
+      <div class="banner-container">
         <CardImg className="dog-image" src={Dogs} alt="Dogs Image" />
+        <CardImgOverlay>
+          <div class="btn-text">
+            <div class="dog-card-text">
+              <CardText>This is your new family!</CardText>
+            </div>
+            <div class="adopt-now-btn">
+              <button type="button" class="btn btn-outline-danger">
+                Adopt Now!
+              </button>
+            </div>
+          </div>
+        </CardImgOverlay>
       </div>
-
-      <CardImgOverlay>
-        <div class="dog-card-text">
-          <CardText>This is your new family!</CardText>
-        </div>
-        <div class="adopt-now-btn">
-          <Button variant="primary">ADOPT NOW</Button>
-        </div>
-      </CardImgOverlay>
     </Card>
   );
 }
