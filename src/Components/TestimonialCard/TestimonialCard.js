13c3342ac81c821d706bf3998e3bb23b8798ff96
NickHudgins <nickhudgins@gmail.com>
2020-02-17 11:28:33 -0500

    Testimonial formatting progress.

--- src/Components/TestimonialCard/TestimonialCard.js
+++ src/Components/TestimonialCard/TestimonialCard.js
@@ -1,23 +1,22 @@
 import React from "react";
 import Testimonial from "../../testimonial.jpg";
-import { Card, CardImg, CardImgOverlay, CardTitle, CardText } from "reactstrap";
+import { Card, CardImg, CardImgOverlay, CardText } from "reactstrap";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./TestimonialCard.css";
-
 
 function TestimonialCard() {
   return (
     <Card className="testimonial-container">
-      <div class="card-header">TESTIMONIAL OF THE MONTH</div>
+      <div class="testimonial-header">TESTIMONIAL OF THE MONTH</div>
       <div class="testimonial-img">
         <CardImg src={Testimonial} alt="Testimonial Card" />
       </div>
-      <div class="testimonial-txt">
         <CardImgOverlay>
+        <div class="testimonial-txt">
           <CardText>Mongi, my best friend who saved my life.</CardText>
-          <button class='button play'></button>
-        </CardImgOverlay>
-      </div>
+        </div>
+        <button class="button-play"></button>
+      </CardImgOverlay>
     </Card>
   );
 }
